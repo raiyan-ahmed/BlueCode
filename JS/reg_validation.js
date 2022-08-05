97c1018cae74424a142e3f43a7e5bc0460e371b5
raiyan-ahmed <rfraiyan123@gmail.com>
2022-08-06 01:52:18 +0600

    fixing error

--- JS/reg_validation.js
+++ JS/reg_validation.js
@@ -7,7 +7,7 @@ function formValidation() {
 
 
     var usernamePattern = /[A-Za-z .]{3,20}/
-    var passPattern = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@%&*£])){8-20}$/
+    var passPattern = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@%&*#£])){6-20}$/
     var emailPattern = /(cse|eee|bba|law|ce)_\d{10}@lus.ac.bd/
     var mobilePattern = /^(\+88)?-?01[3-9]\d{8}$/
 
